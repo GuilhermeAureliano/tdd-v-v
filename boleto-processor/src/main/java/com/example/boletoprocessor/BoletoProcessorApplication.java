041cb9a833484e489d1d9f77ab765ac78ccea900
guilhermeaureliano <guilherme.farias@ccc.ufcg.edu.br>
2024-02-29 15:49:15 -0300

    criação do projeto java

---
+++ boleto-processor/src/main/java/com/example/boletoprocessor/BoletoProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.example.boletoprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoletoProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoletoProcessorApplication.class, args);
+	}
+
+}
---
+++ boleto-processor/src/test/java/com/example/boletoprocessor/BoletoProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boletoprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoletoProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
