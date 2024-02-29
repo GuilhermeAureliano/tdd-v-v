e07b733d6512a3a8a235456b600a542aa3c1c465
guilhermeaureliano <guilherme.farias@ccc.ufcg.edu.br>
2024-02-29 15:59:55 -0300

    test 1: adicionando teste de fatura paga

--- boleto-processor/src/test/java/com/example/boletoprocessor/BoletoProcessorApplicationTests.java
+++ boleto-processor/src/test/java/com/example/boletoprocessor/BoletoProcessorApplicationTests.java
@@ -3,11 +3,28 @@ package com.example.boletoprocessor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class BoletoProcessorApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testFaturaPaga() {
+		Fatura fatura = new Fatura("2024-02-29", 1500, "João Da Silva");
+		Boleto boleto1 = new Boleto("123", "2024-02-28", 500);
+		Boleto boleto2 = new Boleto("651", "2024-02-28", 400);
+		Boleto boleto3 = new Boleto("321", "2024-02-28", 600);
+
+		fatura.addBoleto(boleto1);
+		fatura.addBoleto(boleto2);
+		fatura.addBoleto(boleto3);
+
+		fatura.pagar();
+		assertTrue(true, fatura.isPago());
 	}
 
 }
